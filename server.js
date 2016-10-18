041333214789236d55490641ffa75f99b34652d0
Vikas Kaler <vikas.kalerv@gmail.com>
2016-10-17 22:40:09 -0400

    Language change for about us

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('Private route hiy!');
-		next();
-	},
-	logger: function(req, res, next){
-		var d = new Date();		
-		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + d.toString());
-		next();
-	}
-};
+var middleware = require('middleware.js');
+
 app.use(middleware.logger);
 
 
